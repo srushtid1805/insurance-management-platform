c60db1a8010cbc1b2c6721ec5fa6cd5d581e366d
srushtid1805 <srushtid1805@gmail.com>
2026-08-03 18:14:38 +0530

    Fix LoginPage stylesheet

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { loginUser } from "../services/authService";
-import "./LoginPage.css";
+import "./LoginPageNew.css";
 
 const LoginPage = () => {
   const navigate = useNavigate();
